f92e3af98356b61e95247a11347a038802a59e39
Suyog <120791980+suyogparkhi@users.noreply.github.com>
2024-09-14 13:16:05 +0530

    project setup + tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
